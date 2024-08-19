3ff00ec95fe5bb935fd0fd1338d9247d83b04803
steph <ntuigoodness3@gmail.com>
2024-08-19 15:05:10 +0100

    fix:dependencies

---
+++ src/Components/Button.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Button = ({btnText}) => {
+  return (
+    <>
+    <button className='text-red-500 text-xl md:text-2xl  lg:text-3xl xl:text-4xl  '>{btnText}</button>
+        
+    </>
+  )
+}
+
+export default Button
