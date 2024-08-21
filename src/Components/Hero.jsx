31bdcedf094f3905ae992bac987effd9582f77ba
Wealth <shuaibganiyat20@gmail.com>
2024-08-21 23:47:11 +0100

    update from help

--- src/Components/Hero.jsx
+++ src/Components/Hero.jsx
@@ -3,20 +3,22 @@ import Polygon1 from '../assets/Polygon 1.png'
 import Polygon2 from '../assets/Polygon 2.png'
 import People from '../assets/male and female hero.svg'
 import Animation from '../assets/Animatiion.svg'
+import Rectangle from '../assets/Group 2008.svg'
 import CircularAnimae from './CircularAnimae'
 
 
 const Hero = () => {
   return (
-    <div className='flex flex-col-reverse md:flex-row justify-between m md:max-h-[90vh] bg-primary '>
+    <div className='relative flex flex-col-reverse md:flex-row justify-between m md:max-h-[90vh] bg-primary '>
        <div className="space-y-8 md:w-[25rem] lg:w-[31.5rem] md:ml-8 lg:ml-20 text-white flex flex-col justify-center items-center md:items-start p-4 md:p-0">
         <h3 className=' font-medium text-[1.5rem] md:text-2xl lg:text-[1.75rem]'>Small Business. Big Impact.</h3>
         <h1 className='font-bold text-2xl lg:text-6xl text-center md:text-start'>Build Your Online Presence Today.</h1>
         <button className='w-60 py-3.5 px-16 bg-secondary rounded-[10px] font-medium text-center hover:bg-primary-dark'>Get Started</button>
+        
        </div>
         <div className="relative h-[60vh] md:h-auto ">
-            <img src={Polygon2} className="h-full w-full" alt="" />
-            <div className="absolute z-10 top-[4.8rem] md:top-9 lg:top-11">
+            <img src={Polygon2} className="h-full w-full object-cover" alt="" />
+            <div className="absolute z-10 top-[4.8rem] md:top-9 lg:top-11 -right-2">
             <img src={Polygon1} className="h-full w-full" alt="" />
         </div>
         <div className="absolute z-10 top-[3.5rem] -left-8 hidden md:block">
@@ -26,7 +28,9 @@ const Hero = () => {
             <img src={People} className="h-[24rem] md:h-[12rem] lg:h-full" alt="" />
         </div>
         </div>
-        
+        <div className='absolute top-[28.7rem] left-20 hidden md:block'>
+        <img src={Rectangle} className="" alt="" />
+        </div>
         {/* <CircularAnimae/> */}
 
     </div>
