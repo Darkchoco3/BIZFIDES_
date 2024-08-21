5d5742e6b330b81cf0a856f10587a7f79ad42bc8
maureen <bisiattah@yahoo.com>
2024-08-21 21:42:04 +0100

    sub

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,19 @@
-<<<<<<< HEAD
 import Testimonial from './Components/Testimonial'
 import './App.css'
 import Button from './Components/Button'
-=======
-import "./App.css";
 import Home from "./Pages/Home";
->>>>>>> 8f6120e26358dd003fecc88c4e5c8efcb1d4bf64
 
 function App() {
   return (
     <>
-<<<<<<< HEAD
     {/* <Button btnText={"hello"}/> */}
-      <Testimonial />
-       </>
-  )
-=======
+      
+     
+ 
       <Home />
+      <Testimonial />
     </>
   );
->>>>>>> 8f6120e26358dd003fecc88c4e5c8efcb1d4bf64
 }
 
 export default App;
