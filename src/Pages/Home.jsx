8838b9a329e30cc303d107775dfdd46654d08f43
maureen <bisiattah@yahoo.com>
2024-08-21 21:19:50 +0100

    first update

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import About from '../Components/About'
+import Sub from '../Components/Sub'
 
 const Home = () => {
   return (
     <div>
       <About/>
+      <Sub/>
     </div>
   )
 }
