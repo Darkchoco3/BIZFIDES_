72c85433d19138a429a00ec964d994256dfe6014
Chioma <okekelinda9991@gmail.com>
2024-09-03 23:02:22 -0700

    fixing errors

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -8,7 +8,7 @@ import Testimonials from '../Components/Testimonial'
 import Footer from '../Layouts/Footer'
 import RecentWorks from '../Components/RecentWorks.jsx'
 import Subscribe from '../Components/Subscribe'
-import Sub from '../Components/Sub'
+import Sub from '../Components/BuildProcess.jsx'
 
 const Home = () => {
   return (
