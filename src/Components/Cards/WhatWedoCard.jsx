d706bd5356519a6d25b83983aeab4007cd3bd5cc
Chioma <okekelinda9991@gmail.com>
2024-09-17 00:22:55 -0700

    added animation to whatWeDoCard.jsx

--- src/Components/Cards/WhatWedoCard.jsx
+++ src/Components/Cards/WhatWedoCard.jsx
@@ -1,12 +1,32 @@
+import { motion } from "framer-motion";
+
+const slideInVariants = {
+  hidden: {
+    opacity: 0,
+    x: -100, // Start 100px off-screen to the left
+  },
+  visible: {
+    opacity: 1,
+    x: 0, // End at its original position
+    transition: {
+      duration: 1.5,
+      ease: "easeIn",
+    },
+  },
+};
 const WhatWedoCard = ({what, i}) => {
   return (
-    <div className='min-h-[474px] rounded-[55px] shadow-[0px_0px_12px_8px_rgba(0,0,0,0.02)] p-4 font-inter flex flex-col gap-[24px] border border-neutral-grey-100' key={i}>
+    <motion.div className='min-h-[474px] rounded-[55px] shadow-[0px_0px_12px_8px_rgba(0,0,0,0.02)] p-4 font-inter flex flex-col gap-[24px] border border-neutral-grey-100' key={i}
+    initial="hidden"
+    whileInView="visible"
+        viewport={{ once: true }}
+    variants={slideInVariants}>
       <img src={what.img} alt={what.title} className='rounded-[35px]'/>
       <div className="flex flex-col gap-[7px]">
       <h3 className='text-secondary text-center text-xl font-semibold'>{what.title}</h3>
       <p className='text-sm text-neutral-grey-300 text-center'>{what.desc}</p>
       </div>
-    </div>
+    </motion.div>
   )
 }
 
